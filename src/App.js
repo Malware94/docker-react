8ba7e7f34e1fea51caf7513151ee4d55bc41ecdd
Rajmond Kovács <rajmond.kovacs@dkfkft.hu>
2022-05-04 11:12:46 +0200

    1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>General Kenobi! Well Hello There. Here</p>
+        <p>General Kenobi! Well, Hello There.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
